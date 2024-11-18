139be3db803f501585e97999fe7320a4ce8d40d6
Aditi Murkute <murkuteaditi8@gmail.com>
2024-11-18 23:17:17 +0530

    Create Day1.cpp

---
+++ Day1.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+    for (int i = 0; i < 2; i++) { // Fixed loop condition
+        cout << "hello\n";
+    }
+    for (int i = 0; i < 2; i++) { // Fixed loop condition
+        cout << "world\n";
+    }
+    return 0;
+}
